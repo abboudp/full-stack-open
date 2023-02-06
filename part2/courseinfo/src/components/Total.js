296f4d77516f40eb2e1cdfdd54c4370c20ac82f5
Paul Abboud <abboudpaul112@gmail.com>
2023-02-06 11:55:29 -0800

    completed part2a

---
+++ part2/courseinfo/src/components/Total.js
@@ -0,0 +1,15 @@
+const Total = ({ parts }) => (
+    <>
+        <p>
+            total of {
+                parts.reduce( (sum, part) => 
+                    sum + part.exercises, 
+                    0 
+                )
+            } exercises
+        </p>
+    </>
+)
+
+
+export default Total
